ac2085bf7c89dafbe971e3535305f179f0a2ad1e
Jiří Vrba <jiri@vrba.dev>
2020-12-26 23:41:17 +0100

    Added development database configuration

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :cartographers, Cartographers.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "cartographers",
+  password: "cartographers",
   database: "cartographers_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
