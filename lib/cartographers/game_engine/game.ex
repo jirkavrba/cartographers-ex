e406e8afa1a19b3944c69177320f8442e6f4b665
Jiří Vrba <jiri@vrba.dev>
2020-12-27 15:04:36 +0100

    Added season configurations

--- lib/cartographers/game_engine/game.ex
+++ lib/cartographers/game_engine/game.ex
@@ -13,5 +13,13 @@ defmodule Cartographers.GameEngine.Game do
         active_edicts:  tuple,
     }
     defstruct [:name, :time_threshold, :active_edicts]
+
+    @spec all :: list(Season.t)
+    def all, do: [
+      %Season{ name: :spring, time_threshold: 8, active_edicts: {:a, :b}},
+      %Season{ name: :summer, time_threshold: 8, active_edicts: {:b, :c}},
+      %Season{ name: :autumn, time_threshold: 7, active_edicts: {:c, :d}},
+      %Season{ name: :winter, time_threshold: 6, active_edicts: {:d, :a}}
+    ]
   end
 end
