8746a25812fb2605d16c03574e321b25f4b221e0
Jiří Vrba <jiri@vrba.dev>
2020-12-27 22:27:11 +0100

    Added basic shape operations

--- lib/cartographers/game_engine/shape.ex
+++ lib/cartographers/game_engine/shape.ex
@@ -19,4 +19,94 @@ defmodule Cartographers.GameEngine.Shape do
           definition: list(list(boolean))
         }
   defstruct [:width, :height, :definition]
+
+  alias Cartographers.Utilities.ListRotations
+
+  @doc """
+  Rotates a given shape clockwise
+  For example given:
+
+  .#.
+  .#.
+  ###
+
+  The returned shape will be:
+
+  #..
+  ###
+  #..
+
+  """
+  @spec rotate_clockwise(Shape.t()) :: Shape.t()
+  def rotate_clockwise(original) do
+    %__MODULE__{
+      width: original.height,
+      height: original.width,
+      definition: ListRotations.rotate_right(original.definition)
+    }
+  end
+
+  @doc """
+  Rotates a given shape counter-clockwise
+  For example given:
+
+  .#.
+  .#.
+  ###
+
+  The returned shape will be:
+
+  ..#
+  ###
+  ..#
+
+  """
+  @spec rotate_counterclockwise(Shape.t()) :: Shape.t()
+  def rotate_counterclockwise(original) do
+    %__MODULE__{
+      width: original.height,
+      height: original.width,
+      definition: ListRotations.rotate_left(original.definition)
+    }
+  end
+
+  @doc """
+  Flips a given shape horizontally
+  For example given:
+
+  #..
+  ###
+  #..
+
+  The returned shape will be:
+
+  ..#
+  ###
+  ..#
+
+  """
+  @spec flip_horizontally(Shape.t()) :: Shape.t()
+  def flip_horizontally(original) do
+    %{original | definition: original.definition |> Enum.map(&Enum.reverse/1)}
+  end
+
+  @doc """
+  Flips a given shape vertically
+  For example given:
+
+  .#.
+  .#.
+  ###
+
+  The returned shape will be:
+
+  ###
+  .#.
+  .#.
+
+  """
+  @spec flip_vertically(Shape.t()) :: Shape.t()
+  def flip_vertically(original) do
+    %{original | definition: original.definition |> Enum.reverse}
+  end
 end
---
+++ lib/cartographers/utilities/list_rotations.ex
@@ -0,0 +1,5 @@
+defmodule Cartographers.Utilities.ListRotations do
+  # TODO: Implement those methods
+  def rotate_left(list), do: list
+  def rotate_right(list), do: list
+end
