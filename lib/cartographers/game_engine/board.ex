8186c57c15da0acaa735cd197e0112cc36c0b5c5
Jiří Vrba <jiri@vrba.dev>
2020-12-27 14:03:25 +0100

    Green field goes brrr

--- lib/cartographers/game_engine/board.ex
+++
@@ -1,8 +0,0 @@
-defmodule Cartographers.GameEngine.Board do
-  @moduledoc """
-  Module containing structures and functions for working with game board.
-  The game board is owned by player and contains 11x11 tiles.
-
-  The board also contains ruins and mountains in addition to user-placed pieces
-  """
-end
