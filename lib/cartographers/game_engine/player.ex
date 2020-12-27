f6ddfe7c90acf41d8bf629d75dc414f48ddc92f9
Jiří Vrba <jiri@vrba.dev>
2020-12-27 14:43:34 +0100

    Added map sheet type reference to Player

--- lib/cartographers/game_engine/player.ex
+++ lib/cartographers/game_engine/player.ex
@@ -3,12 +3,11 @@ defmodule Cartographers.GameEngine.Player do
   Each player has an unique ID, username (which is unique within every individual game).
   Also, every player has its own map sheet which can be accessed through this structure
   """
-
   @enforce_keys [:id, :username]
   @type t :: %__MODULE__{
     id: integer,
     username: String.t,
-    map_sheet: nil # TODO: Add a MapSheet structure reference when implemented
+    map_sheet: MapSheet.t
   }
   defstruct [:id, :username, :map_sheet]
 end
