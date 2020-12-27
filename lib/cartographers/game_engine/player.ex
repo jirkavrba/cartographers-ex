6df12457875cbbd557c3f74f6489bd0b54b77a70
Jiří Vrba <jiri@vrba.dev>
2020-12-27 14:22:23 +0100

    Added work in progress Player structure

---
+++ lib/cartographers/game_engine/player.ex
@@ -0,0 +1,14 @@
+defmodule Cartographers.GameEngine.Player do
+  @moduledoc """
+  Each player has an unique ID, username (which is unique within every individual game).
+  Also, every player has its own map sheet which can be accessed through this structure
+  """
+
+  @enforce_keys [:id, :username]
+  @type t :: %__MODULE__{
+    id: integer,
+    username: String.t,
+    map_sheet: nil # TODO: Add a MapSheet structure reference when implemented
+  }
+  defstruct [:id, :username, :map_sheet]
+end
