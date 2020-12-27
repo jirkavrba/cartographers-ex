3cfbefa802d825dee918a642c83c419b68921013
Jiří Vrba <jiri@vrba.dev>
2020-12-27 14:42:54 +0100

    Added basic implementation of the map sheet

---
+++ lib/cartographers/game_engine/map_sheet.ex
@@ -0,0 +1,37 @@
+defmodule Cartographers.GameEngine.MapSheet do
+  @moduledoc """
+  Representation of the map sheet that every player receives at the beggining of the game
+  """
+  @enforce_keys [:tiles, :mountains, :ruins]
+  @type t :: %__MODULE__{
+    tiles: list(list(Tile.t)), # A 11x11 matrix of tiles representing the paper sheet
+    ruins: list(Position.t)
+  }
+  defstruct [:tiles, :ruins]
+
+  defmodule Position do
+    @moduledoc """
+    Wrapper for position on the map sheet.
+    In the desktop version the x is represented by numbers 1 .. 11 and y is a letter within A .. K
+    """
+    @enforce_keys [:x, :y]
+    @type t :: %__MODULE__{
+      x: integer,
+      y: integer
+    }
+    defstruct [:x, :y]
+  end
+
+  defmodule Tile do
+    @moduledoc """
+    Wrapper for a single map sheet tile. Contains information about its location and material.
+    There is a special material for unoccupied tiles: `:empty`
+    """
+    @enforce_keys [:material, :position]
+    @type t :: %__MODULE__{
+      material: :empty | :forest | :village | :farm | :water | :monster | :mountain,
+      position: Position.t
+    }
+    defstruct [:material, :position]
+  end
+end
