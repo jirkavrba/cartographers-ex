db13e561ad201686b5ab1e94fb26c0a4d47094a5
Jiří Vrba <jiri@vrba.dev>
2020-12-28 21:43:14 +0100

    I should stop writing tests drunk

--- test/cartographers/utilities/list_rotation_test.exs
+++ test/cartographers/utilities/list_rotation_test.exs
@@ -2,17 +2,17 @@ defmodule Cartographers.Utilities.ListRotationsTest do
   use ExUnit.Case
   import Cartographers.Utilities.ListRotations
 
-  test "test ListRotations.left_rotation/1" do
-    assert rotate_left([]) == []
-    assert rotate_left([[1, 2, 3]]) == [[1], [2], [3]]
-    assert rotate_left([[1, 2], [3, 4]]) == [[3, 1], [4, 2]]
-    assert rotate_left([[1, 2], [3, 4], [5, 6]]) == [[5, 3, 1], [6, 4, 2]]
-  end
-
-  test "test ListRotations.right_rotation/1" do
+  test "test ListRotations.rotate_left/1" do
     assert rotate_left([]) == []
     assert rotate_left([[1, 2, 3]]) == [[3], [2], [1]]
-    assert rotate_left([[1, 2], [3, 4]]) == [[1, 4], [1, 3]]
-    assert rotate_left([[1, 2], [3, 4], [5, 6]]) == [[5, 3, 1], [6, 4, 2]]
+    assert rotate_left([[1, 2], [3, 4]]) == [[2, 4], [1, 3]]
+    assert rotate_left([[1, 2], [3, 4], [5, 6]]) == [[2, 4, 6], [1, 3, 5]]
+  end
+
+  test "test ListRotations.rotate_right/1" do
+    assert rotate_right([]) == []
+    assert rotate_right([[1, 2, 3]]) == [[1], [2], [3]]
+    assert rotate_right([[1, 2], [3, 4]]) == [[3, 1], [4, 2]]
+    assert rotate_right([[1, 2], [3, 4], [5, 6]]) == [[5, 3, 1], [6, 4, 2]]
   end
 end
