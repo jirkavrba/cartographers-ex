9319a29e68ba0fa52c940265270b94e76cb0e9ea
Jiří Vrba <jiri@vrba.dev>
2020-12-30 11:03:38 +0100

    Added implementation of list rotations

--- lib/cartographers/utilities/list_rotations.ex
+++ lib/cartographers/utilities/list_rotations.ex
@@ -1,5 +1,57 @@
 defmodule Cartographers.Utilities.ListRotations do
-  # TODO: Implement those methods
-  def rotate_left(list), do: list
-  def rotate_right(list), do: list
+  # TODO: Optimize those algoritms to use tail recursion
+
+  @doc """
+  Rotates a given list to the left (clockwise)
+  For example, given a list that looks like:
+  [
+    [1, 2, 3],
+    [4, 5, 6]
+  ]
+
+  the output would be:
+  [
+    [3, 6],
+    [2, 5],
+    [1, 4]
+  ]
+
+  """
+  def rotate_left([]), do: []
+  def rotate_left(list) when is_list(list) do
+    base = list |> Enum.map(&List.last/1)
+    rest = list |> Enum.map(&Enum.drop(&1, -1))
+
+    if List.first(rest) == [],
+      do: [base],
+      else: [base] ++ rotate_left(rest)
+  end
+  def rotate_left(_), do: raise ArgumentError
+
+  @doc """
+  Rotates a given list to the right (clockwise)
+  For example, given a list that looks like:
+  [
+    [1, 2, 3],
+    [4, 5, 6]
+  ]
+
+  the output would be:
+  [
+    [4, 1],
+    [5, 2],
+    [6, 3]
+  ]
+
+  """
+  def rotate_right([]), do: []
+  def rotate_right(list) when is_list(list) do
+    base = list |> Enum.map(&List.first/1)
+    rest = list |> Enum.map(&Enum.drop(&1, 1))
+
+    if List.last(rest) == [],
+      do: [Enum.reverse(base)],
+      else: [Enum.reverse(base)] ++ rotate_right(rest)
+  end
+  def rotate_right(_), do: raise ArgumentError
 end
--- test/cartographers/utilities/list_rotation_test.exs
+++ test/cartographers/utilities/list_rotation_test.exs
@@ -7,6 +7,7 @@ defmodule Cartographers.Utilities.ListRotationsTest do
     assert rotate_left([[1, 2, 3]]) == [[3], [2], [1]]
     assert rotate_left([[1, 2], [3, 4]]) == [[2, 4], [1, 3]]
     assert rotate_left([[1, 2], [3, 4], [5, 6]]) == [[2, 4, 6], [1, 3, 5]]
+    assert rotate_left([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == [[3, 6, 9], [2, 5, 8], [1, 4, 7]]
   end
 
   test "test ListRotations.rotate_right/1" do
@@ -14,5 +15,6 @@ defmodule Cartographers.Utilities.ListRotationsTest do
     assert rotate_right([[1, 2, 3]]) == [[1], [2], [3]]
     assert rotate_right([[1, 2], [3, 4]]) == [[3, 1], [4, 2]]
     assert rotate_right([[1, 2], [3, 4], [5, 6]]) == [[5, 3, 1], [6, 4, 2]]
+    assert rotate_right([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
   end
 end
