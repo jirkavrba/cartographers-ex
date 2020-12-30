fe9181893169b021992e02bd44d28e29048c777c
Jiří Vrba <jiri@vrba.dev>
2020-12-30 11:27:46 +0100

    Added edict cards and behaviour specification for scoring rules

--- lib/cartographers/game_engine/cards.ex
+++ lib/cartographers/game_engine/cards.ex
@@ -1,5 +1,6 @@
 defmodule Cartographers.GameEngine.Cards do
   alias Cartographers.GameEngine.Shape
+  alias Cartographers.GameEngine.MapSheet
 
   defmodule ExploreCard do
     defmodule ExploreCardVariant do
@@ -24,5 +25,29 @@ defmodule Cartographers.GameEngine.Cards do
             time_value: integer
           }
     defstruct [:name, :variants, :time_value]
+  end
+
+  defmodule EdictCard do
+    @moduledoc """
+    Edict cards specify scoring rules from categories A, B, C and D.
+    At the end of every season, 2 edict cards are evaluated (which cards are evaluated depends on the current season) and
+    it provides an integer score of every player based on his map sheet and the scoring rule implementation.
+    """
+
+    defmodule ScoringRule do
+      @type t :: __MODULE__
+      @doc """
+      Calculates an integer score based on the board game cards (eg. 1 point for every row and column with a forest card).
+      """
+      @callback calculate_score(MapSheet.t()) :: integer
+    end
+
+    @enforce_keys [:category, :name, :rule]
+    @type t ::%__MODULE__{
+      category: :a | :b | :c | :d,
+      name: String.t(),
+      rule: ScoringRule.t()
+    }
+    defstruct [:category, :name, :rule]
   end
 end
