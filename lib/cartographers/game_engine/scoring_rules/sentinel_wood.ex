e117397dd31bf2b800201334461a4ca0524b4df6
Jiří Vrba <jiri@vrba.dev>
2021-01-03 21:00:51 +0100

    Added implementation of SentinelWood scoring rule

--- lib/cartographers/game_engine/scoring_rules/sentinel_wood.ex
+++ lib/cartographers/game_engine/scoring_rules/sentinel_wood.ex
@@ -8,7 +8,14 @@ defmodule Cartographers.GameEngine.ScoringRules.SentinelWood do
   @behaviour Cartographers.GameEngine.Cards.EdictCard.ScoringRule
 
   @spec calculate_score(MapSheet.t()) :: integer
-  def calculate_score(_map_sheet) do
-    0
+  def calculate_score(map_sheet) do
+    (
+      MapSheet.row(map_sheet, 0) ++
+      MapSheet.row(map_sheet, 10) ++
+      MapSheet.column(map_sheet, 0) ++
+      MapSheet.column(map_sheet, 10)
+    )
+    |> Enum.uniq()
+    |> Enum.count(&(&1.material == :forest))
   end
 end
